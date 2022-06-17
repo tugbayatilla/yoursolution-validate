8c428b555c8ff11e4210bfac5fa3de3760edbaa6
Tugbay Atilla <Tugbay Atilla tatilla@igus.net>
2022-06-17 20:53:07 +0200

    feat: implement validate according the test

--- YourSolution.cs
+++ YourSolution.cs
@@ -4,7 +4,7 @@ namespace yoursolution_validate
     {
         internal static bool Validate(string v1, string v2)
         {
-            throw new NotImplementedException();
+            return v1.Contains(v2[0]) && v1.Contains(v2[1]);
         }
     }
 }
