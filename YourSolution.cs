5c4a615f518fde954c40992ca8270da7598187d1
Tugbay Atilla <Tugbay Atilla tatilla@igus.net>
2022-06-19 18:30:14 +0200

    refactor: add Sign class to get rid of Tuple

--- YourSolution.cs
+++ YourSolution.cs
@@ -3,21 +3,21 @@ namespace yoursolution_validate
     internal class YourSolution
     {
         internal static Action<string>? _logger;
-        
+
         internal static bool Validate(string v1, string v2)
         {
-            var signs = new List<Tuple<char, char>>();
-            for (int i = 0; i < v2.Length; i+=2)
+            var signs = new List<Sign>();
+            for (int i = 0; i < v2.Length; i += 2)
             {
-                var sign = new Tuple<char, char>(v2[i], v2[i+1]);
-                _logger?.Invoke($"{sign.Item1}{sign.Item2}");        
+                var sign = new Sign(v2[i], v2[i + 1]);
+                _logger?.Invoke($"{sign.OpenChar}{sign.CloseChar}");
                 signs.Add(sign);
             }
-            
+
             foreach (var s in signs)
             {
-                int indexOfOpenChar = v1.IndexOf(s.Item1);
-                int indexOfCloseChar = v1.IndexOf(s.Item2);
+                int indexOfOpenChar = v1.IndexOf(s.OpenChar);
+                int indexOfCloseChar = v1.IndexOf(s.CloseChar);
 
                 if (indexOfOpenChar != -1 && indexOfCloseChar != -1)
                 {
@@ -31,6 +31,17 @@ namespace yoursolution_validate
 
             return true;
 
+        }
+
+        class Sign
+        {
+            public Sign(char openChar, char closeChar)
+            {
+                this.OpenChar = openChar;
+                this.CloseChar = closeChar;
+            }
+            public char OpenChar { get; set; }
+            public char CloseChar { get; set; }
         }
     }
 }
